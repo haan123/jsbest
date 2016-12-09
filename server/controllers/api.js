6f7e6d6086c5dd1dd28778a28f8c2945447f69ca
An Ha <an.ha@amblique.com>
2016-12-09 16:59:57 +0700

    setup server

---
+++ server/controllers/api.js
@@ -0,0 +1,38 @@
+var _ = require('lodash');
+
+/**
+ * Split into declaration and initialization for better startup performance.
+ */
+var Github;
+
+/**
+ * GET /api
+ * List of API examples.
+ */
+exports.getApi = function(req, res) {
+  res.render('api/index', {
+    title: 'API Examples'
+  });
+};
+
+/**
+ * GET /api/github
+ * GitHub API Example.
+ */
+exports.getGithub = function(req, res, next) {
+  Github = require('github-api');
+
+  var token = _.find(req.user.tokens, { kind: 'github' });
+  var github = new Github({ token: token.accessToken });
+  var repo = github.getRepo('sahat', 'requirejs-library');
+  repo.getDetails(function(err, repo) {
+    if (err) {
+      return next(err);
+    }
+    res.render('api/github', {
+      title: 'GitHub API',
+      repo: repo
+    });
+  });
+
+};
